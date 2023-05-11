688abb79768e0dc8817188493095a8ccac746708
Kristi K <kristik06@outlook.com>
2023-05-11 15:32:15 +0200

    Create smooth_to_rough.py

---
+++ smooth_to_rough.py
@@ -0,0 +1,34 @@
+"""
+Purpose: Convert PBR Smoothness maps to Roughness.
+Smoothness maps are commonly used in Unity Engine.
+"""
+
+import sys
+from PIL import Image, ImageOps
+from pathlib import Path
+
+# Get the path to the image file
+if len(sys.argv) > 1:
+    smooth_path = Path(sys.argv[1])
+else:
+    print(f'Usage: python {Path(__file__).name} <image_file>')
+    input()
+    sys.exit()
+
+# Open the image
+try:
+    with Image.open(smooth_path) as smooth:
+        rough_path = smooth_path.with_stem(
+            smooth_path.stem.lower().removesuffix("smoothness")
+            .removesuffix("smooth")
+            .removesuffix("_")
+            + "_rough"
+        )
+        # Convert the image from smoothness to roughness
+        rough = ImageOps.invert(smooth.convert("L"))
+        rough.save(rough_path)
+        print(f'{smooth_path} converted to {rough_path}')
+
+except IOError as e:
+    input()
+    print(f'Error: Could not open {image_path}, {e}')
